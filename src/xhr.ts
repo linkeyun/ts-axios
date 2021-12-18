f0aebcbf4c855ae3332973fa562a2e3665276e36
linkeyun <714997079@qq.com>
2021-12-18 16:32:29 +0800

    处理请求header

--- src/xhr.ts
+++ src/xhr.ts
@@ -1,11 +1,17 @@
 import { AxiosRequestConfig } from './types'
 
 export default function xhr(config: AxiosRequestConfig) {
-  const { data = null, url, method = 'get' } = config
+  const { data = null, url, method = 'get', headers } = config
 
   const request = new XMLHttpRequest()
 
   request.open(method.toUpperCase(), url, true)
+
+  Object.keys(headers).forEach(name => {
+    data === null && name.toLocaleLowerCase() === 'content-type'
+      ? delete headers[name]
+      : request.setRequestHeader(name, headers[name])
+  })
 
   request.send(data)
 }
