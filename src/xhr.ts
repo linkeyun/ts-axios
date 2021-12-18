38bc4e1c6227fec3a483dffba1dea8e3e4e8509e
linkeyun <714997079@qq.com>
2021-12-18 17:03:48 +0800

    处理响应数据header/data

--- src/xhr.ts
+++ src/xhr.ts
@@ -1,17 +1,41 @@
-import { AxiosRequestConfig } from './types'
+import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
+import { parseHeaders } from './helpers/header'
 
-export default function xhr(config: AxiosRequestConfig) {
-  const { data = null, url, method = 'get', headers } = config
+export default function xhr(config: AxiosRequestConfig): AxiosPromise {
+  return new Promise(resolve => {
+    const { data = null, url, method = 'get', headers, responseType } = config
 
-  const request = new XMLHttpRequest()
+    const request = new XMLHttpRequest()
 
-  request.open(method.toUpperCase(), url, true)
+    if (responseType) {
+      request.responseType = responseType
+    }
 
-  Object.keys(headers).forEach(name => {
-    data === null && name.toLocaleLowerCase() === 'content-type'
-      ? delete headers[name]
-      : request.setRequestHeader(name, headers[name])
+    request.open(method.toUpperCase(), url, true)
+
+    request.onreadystatechange = function handleLoad() {
+      if (request.readyState !== 4) return
+
+      const responseHeaders = parseHeaders(request.getAllResponseHeaders())
+      const responseData = responseType !== 'text' ? request.response : request.responseText
+      const response: AxiosResponse = {
+        data: responseData,
+        status: request.status,
+        statusText: request.statusText,
+        headers: responseHeaders,
+        config,
+        request
+      }
+
+      resolve(response)
+    }
+
+    Object.keys(headers).forEach(name => {
+      data === null && name.toLocaleLowerCase() === 'content-type'
+        ? delete headers[name]
+        : request.setRequestHeader(name, headers[name])
+    })
+
+    request.send(data)
   })
-
-  request.send(data)
 }
